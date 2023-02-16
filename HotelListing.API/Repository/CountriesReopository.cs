e6ba132879095ec13a7e289bf3efc520066f3517
Anay Pujari <anay0222@outlook.com>
2023-02-16 13:33:48 +0530

    Rest API Development Best Practices. Automapper and Repositories

---
+++ HotelListing.API/Repository/CountriesReopository.cs
@@ -0,0 +1,22 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class CountriesReopository : GenericRepository<Country>, ICountriesRepositories
+    {
+        private readonly HotelListingDbContext _context;
+
+        public CountriesReopository(HotelListingDbContext context) : base(context)
+        {
+            this._context = context;
+        }
+
+        public async Task<Country> GetDetails(int id)
+        {
+            return await _context.Countries.Include(q=>q.Hotels)
+                .FirstOrDefaultAsync(q=>q.Id== id);
+        }
+    }
+}
